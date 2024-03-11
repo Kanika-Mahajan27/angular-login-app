d37ac1322347503ecdd2d438ffb0c881e11a8093
Gaurav Singh <gaurav.singh10@nagarro.com>
2024-03-11 10:02:37 +0530

    add null check

--- src/app/validators/noWhitespaceValidator.ts
+++ src/app/validators/noWhitespaceValidator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidatorFn } from "@angular/forms";
 
 export function noWhitespaceValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
-      if (control.value.trim() === '') {
+      if (control.value && control.value.trim() === '') {
         return { 'required': true };
       }
       return null;
